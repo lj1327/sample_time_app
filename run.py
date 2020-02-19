9d5883f1a2edb2dfde8f8e3cac0c16d49ea5d9e9
lj1327 <61091375+lj1327@users.noreply.github.com>
2020-02-19 09:42:27 -0500

    Add files via upload

---
+++ run.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import datetime
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello world!'
+
+@app.route('/time')
+def time():
+    curtime = str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    return curtime
+
+app.run(host='0.0.0.0',
+        port=8080,
+        debug=True)
+
